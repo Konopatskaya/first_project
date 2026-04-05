8741cd01609adf661694e4d702c76462b4ec0297
Ekaterina Konopackaya <geba57@mail.ru>
2026-04-05 17:17:20 +0400

    feat(main.py) Добавлена функция Author. Вывести имя автора

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 print('Hello from repository! All is good!')
+
+from dotenv import load_dotenv 
+import os  
+def print_author():
+    # Загружаем переменные из файла .env
+    
+    load_dotenv(dotenv_path='C:/Kat/DataMining/work_terminal/project2/.env')
+	    
+    # Получаем значение переменной AUTHOR и присваиваем его переменной author
+    author = os.getenv('AUTHOR')
+    
+    print(f"Автор проекта: {author}")
+	
+# Вызов функции для проверки её работы
+print_author()
